776fd873d116046b97bf34db41bf4fc221325aed
Nino Lomidze <118027268+ninolomidze1@users.noreply.github.com>
2023-01-17 00:21:19 +0400

    javaScript1
    workshop1

---
+++ script.js
@@ -0,0 +1,26 @@
+// problem 1
+// შექმენით რამდენიმე ცვლადი სადაც შეინახავთ თქვენს სახელს, გვარს, სქესს და დაბადების წელს.
+
+let name = "Nino";
+var surname = "Lomidze";
+const gender = "female";
+const dateOfBirth = "2003";
+
+console.log(name);
+console.log(surname);
+console.log(gender);
+console.log(dateOfBirth);
+
+// problem 2
+// რამდენ ექსფრეშენს ხედავთ მოცემულ კოდში?
+
+// (100 - (10+15)/5)*3
+
+// ამ კოდში გვხვდება  9 ექსფრეშენი.
+
+// problem 3
+// შეინახეთ ცვლადში და შემდეგ კონსოლში დაბეჭდეთ რას უდრის 101-ის 2-ზე გაყოფისას მიღებული ნაშთი (კონსოლში მნიშვნელობის გამოსატანად ვიყენებთ console.log() ფუნქციას).
+
+let number = 101;
+
+console.log(101 % 2);
